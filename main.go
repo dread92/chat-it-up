25fb77f7c73dee4b73ea08980d21392b8cf1183f
David Read <dread+github@dread-space.com>
2017-07-28 04:37:26 -0400

    run gofmt and it fixed some spaces and reordered the imports

--- main.go
+++ main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
-	"text/template"
 	"path/filepath"
 	"sync"
+	"text/template"
 )
 
 type templateHandler struct {
-	once 	 sync.Once
+	once     sync.Once
 	filename string
-	templ 	 *template.Template
+	templ    *template.Template
 }
 
-func (t *templateHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func(){
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",t.filename)))
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w,nil)
+	t.templ.Execute(w, nil)
 }
 
 func main() {
-	http.Handle("/",&templateHandler{filename: "chat.html"})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
